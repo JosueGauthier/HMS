75e1d31bc5fe4b9057c01bb43ddedd1ef24aed44
JosueGauthier <josue.gauthier@gmail.com>
2022-12-10 15:17:01 +0100

    or2

---
+++ project/or2.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity or2 IS
+    port (in1,in2: in  std_logic; 
+          or_out:  out std_logic);
+end or2;
+
+architecture structural of or2 is
+begin    
+    or_out <= in1 or in2;    
+end structural;
